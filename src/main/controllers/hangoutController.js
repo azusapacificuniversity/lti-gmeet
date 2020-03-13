cf1acbfa198d20721d99ccf61ad05b1683f2a4d3
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-13 15:55:27 -0700

    Edit Response

--- src/main/controllers/hangoutController.js
+++ src/main/controllers/hangoutController.js
@@ -3,7 +3,7 @@ exports.gMeet = async function gMeet(context) {
   if (context.requestBody.custom_canvas_course_id !== null && context.requestBody.custom_canvas_course_id !== undefined) {
     context.res
       .status(200)
-      .setBody('<html>  <body>  <h1>YouTube Searcher</h1>  <p>Search for a YouTube video below. The first result  is what will be shown whenever anyone loads this app.  </p>  <form id="form">    <input type="text" name="query" id="query"/><button type="submit">I\'m Feeling Lucky < /button>  </form > ');
+      .setBody('<html>  <body>  <h1>YouTube Searcher</h1>  <p>Search for a YouTube video below. The first result  is what will be shown whenever anyone loads this app.  </p>  <form id="form">    <input type="text" name="query" id="query"/><button type="submit">I\'m Feeling Lucky < /button>  </form > </body> </html> ');
   } else {
     context.res
       .status(400)
