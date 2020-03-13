10e31d9b86c37be108b8b596c71aea212a383653
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-13 15:19:28 -0700

    Modify response

--- src/main/controllers/hangoutController.js
+++ src/main/controllers/hangoutController.js
@@ -3,7 +3,8 @@ exports.gMeet = async function gMeet(context) {
   if (context.requestBody.custom_canvas_course_id !== null && context.requestBody.custom_canvas_course_id !== undefined) {
     context.res
       .status(200)
-      .setBody(context.requestBody.custom_canvas_course_id);
+      .setBody('<form action="https://google.com/' +
+        context.requestBody.custom_canvas_course_id + '"> <inputtype="submit" value="Go to Google"/> </form>');
   } else {
     context.res
       .status(400)
