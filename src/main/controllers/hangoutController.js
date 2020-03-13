ba6dcdd6f401ac19d7c6093ad4488d8a995cf32f
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-13 16:43:02 -0700

    save xml config, modify response

--- src/main/controllers/hangoutController.js
+++ src/main/controllers/hangoutController.js
@@ -1,12 +1,5 @@
 exports.gMeet = async function gMeet(context) {
-  console.log(context.res);
-  if (context.requestBody.custom_canvas_course_id !== null && context.requestBody.custom_canvas_course_id !== undefined) {
-    context.res
-      .status(200)
-      .setBody('<html>  <body>  <h1>YouTube Searcher</h1>  <p>Search for a YouTube video below. The first result  is what will be shown whenever anyone loads this app.  </p>  <form id="form">    <input type="text" name="query" id="query"/><button type="submit">I\'m Feeling Lucky < /button>  </form > </body> </html> ');
-  } else {
-    context.res
-      .status(400)
-      .setBody('Class Code not specified');
-  }
+  context.res
+    .status(301)
+    .setHeader('Location', 'https://www.google.com/search?client=firefox-b-1-d&ei=NApsXvfvL9bH-gTijbOACA&q=' + context.requestBody.custom_canvas_course_id);
 };
