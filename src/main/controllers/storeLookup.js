2bbeb12e0ccbeb9f619bbed5167c71f1405b3ae9
Gregory Montilla <gcmontilla5@gmail.com>
2020-03-13 14:26:34 -0700

    Removed Canonical

--- src/main/controllers/storeLookup.js
+++ src/main/controllers/storeLookup.js
@@ -3,16 +3,9 @@ class StoreLookupRepo {
         this.knex = _knex;
     }
     saveCourse(event) {
-        return this.toCanonical(this.knex
+        return this.knex
             .insert({class_id: event.id, link: event.hangoutLink, phone: event.phone, pin: event.pin})
             .into('canvas_lti_google_meets')
-    }
-    toCanonical(data) {
-        return data.map(p => ({
-            apuId: p.EMPLID,
-            firstName: p.FIRST_NAME,
-            apuEmail: p.EMAIL_ADDR
-        }));
     }
 }
 
