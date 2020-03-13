879bcea2a1f5f2f8256b1712c9c0ebea7a3e24c5
Gregory Montilla <gcmontilla5@gmail.com>
2020-03-13 14:39:32 -0700

    saveCourse, findByClassId

--- src/main/controllers/storeLookup.js
+++ src/main/controllers/storeLookup.js
@@ -1,11 +1,18 @@
 class StoreLookupRepo {
     constructor(_knex) {
         this.knex = _knex;
+        this.db = 'canvas_lti_google_meets';
     }
+
     saveCourse(event) {
         return this.knex
             .insert({class_id: event.id, link: event.hangoutLink, phone: event.phone, pin: event.pin})
-            .into('canvas_lti_google_meets')
+            .into(this.db);
+    }
+
+    findByClassId(classId) {
+        return this.knex
+            .select().from(this.db).where('class_id', classId);
     }
 }
 
