e494dcf804eb493cc3c3d0efc0fd0ab4ddd16526
Gregory Montilla <gcmontilla5@gmail.com>
2020-03-13 11:18:32 -0700

    Created class for google calendar

---
+++ src/main/classes/googleCalendar.js
@@ -0,0 +1,73 @@
+const { google } = require("googleapis");
+const SCOPES = ["https://www.googleapis.com/auth/calendar"];
+const USER = "imtexpdeveloper@apu.edu";
+
+class googleCalendar {
+    /**
+     * 
+     * @param {} config 
+     * @param {String} user 
+     */
+    constructor(config, user) {
+        this.jwtClient = new google.auth.JWT(
+            config.client_email,
+            null,
+            config.private_key,
+            SCOPES,
+            user
+        );
+        
+        this.calendar = google.calendar({
+            version: "v3",
+            auth: this.jwtClient
+        });
+    }
+    
+    /**
+     * Creates an event using Google Calendar API and returns the event
+     * 
+     * @param {String} calendarId Id of calendar where events will be stored
+     * @param {String} requestId Random client generated id 
+     * @param {String} event Object of event that is being stored
+     */
+    async insert(calendarId, requestId, event) {        
+        this.calendar.events.insert({
+            calendarId: calendarId,
+            resource: event,
+            conferenceDataVersion: 1
+        }).then(event => {
+            return event.data
+        }).catch(err => {
+            return err
+        })
+    }
+
+    /**
+     * Creates event from Canvas data to be used with Google Calendar API
+     * 
+     * @param {Object} details 
+     */
+    createEvent(details) {
+        const today = new Date()
+        
+        let event  = {
+            summary: details.sumary,
+            description: details.description,
+            start: {
+                dateTime: today.toISOString(),
+                timeZone: "America/Los_Angeles"
+            },
+            end: {
+                dateTime: today.toISOString(),
+                timeZone: "America/Los_Angeles"
+            },
+            conferenceData: {
+                createRequest: {
+                        requestId: requestId
+                }
+            }
+        }
+
+        return event;
+    }
+}
