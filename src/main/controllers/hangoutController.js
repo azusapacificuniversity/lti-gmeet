ed5939d38686d6a14ec1d276b53c4eeac4d20a09
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-17 08:19:29 -0700

    Added loggers to determine what data is being passed

--- src/main/controllers/hangoutController.js
+++ src/main/controllers/hangoutController.js
@@ -2,8 +2,13 @@ const env = require("../env.js");
 const LtiMeet = require('../classes/LtiMeet.js');
 
 exports.gMeet = async function gMeet(context) {
+    console.log(context);
+    console.log("\n");
     ltiMeet = new LtiMeet(env.createGCal(), env.createStoreRepo(context.knex));
+    console.log(ltiMeet);
+    console.log("\n");
     let meet = await ltiMeet.meetByClassId(context.requestBody.custom_canvas_course_id);
+    console.log(meet);
     context.res
         .status(301)
         .setHeader('Location', meet.link);
