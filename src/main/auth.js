80b1b17cdf86c44579aef60a1e2fcf48bcbd00d3
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-20 09:21:07 -0700

    Test google's version of getToken

--- src/main/auth.js
+++ src/main/auth.js
@@ -22,8 +22,16 @@ class Auth {
     }
 
     async getToken(code) {
-        console.log(code);
-        return (await this.oAuth2Client.getToken(code));
+        this.oAuth2Client.getToken(code, (err, tokens) => {
+            if (err) {
+                console.error('Error getting oAuth tokens:');
+                console.log(code);
+                throw err;
+            }
+            this.oAuth2Client.credentials = tokens;
+            console.log('Authentication successful!');
+        });
+        // return (await this.oAuth2Client.getToken(code));
     }
 
     setCredentials(tokens) {
