5f142519aaa7885b8bb157fe66aaa6cf365babc8
Seth Wheeler (Student) <swheeler17@apu.edu>
2020-03-27 11:47:58 -0700

    Bugfix with knexController

--- src/main/controllers/hangoutController.js
+++ src/main/controllers/hangoutController.js
@@ -4,13 +4,15 @@ const fs = require('fs');
 const path = require('path');
 
 exports.ltiHtmlPost = function(context) {
+    // console.log(context);
     context.res.setHeader('Content-Type', 'text/html');
-    return lti(context.knex, context.requestBody.custom_canvas_course_id, context.requestBody.roles, context);
+    return lti(context.extraContext.knex, context.requestBody.custom_canvas_course_id, context.requestBody.roles, context);
 }
 
 exports.ltiHtmlGet = function(context) {
+    // console.log(context);
     context.res.setHeader('Content-Type', 'text/html');
-    return lti(context.knex, context.params.query.course_id, context.params.query.roles, context);
+    return lti(context.extraContext.knex, context.params.query.course_id, context.params.query.roles, context);
 }
 
 async function lti(knex, course_id, roles = 'student', context) {
